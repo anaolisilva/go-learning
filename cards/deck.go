eede62839da2d196674096701fc4ce6a038f1514
anaolisilva <ana.oli@cora.com.br>
2022-02-22 17:29:48 -0300

    Removing comments

--- cards/deck.go
+++ cards/deck.go
@@ -2,13 +2,8 @@ package main
 
 import "fmt"
 
-//Declarando o novo tipo: deck, que funciona como um slice de string.
 type deck []string
 
-
-//Não tem receiver porque está justamente criando uma variável do tipo deck, ou seja
-//não faria sentido que para criar um novo deck precisássemos de um já pronto.
-//Recebe um parâmetro caso desejem que tenha coringas.
 func newDeck(joker bool) deck {
 	cards := deck{}
 
@@ -16,7 +11,6 @@ func newDeck(joker bool) deck {
 
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
-	//Iterando pelos nipes e valores para criar todas as combinações de cartas.
 	for _, suit := range cardSuits{
 		for _, value := range cardValues {
 			cards = append(cards, value + " of " + suit)
@@ -30,16 +24,12 @@ func newDeck(joker bool) deck {
 	return cards
 }
 
-
-//Lembrar que (d deck) é um receiver, que torna a função disponíveis para
-//variáveis do tipo deck.
 func (d deck) print() {
 	for i, singleCard := range d {
 		fmt.Println(i, singleCard)
 	}
 }
 
-//Função que retorna dois recursos diferentes, ambos do tipo deck.
 func deal (d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -4,16 +4,13 @@ import "fmt"
 
 func main() {
 
-	//Declarando e populando um novo tipo deck.
 	cards := newDeck(false)
 
 	hand, remainingCards := deal(cards, 3)
 
 	fmt.Println("Hand dealt:")
-	//Chamando a função criada no tipo deck
 	hand.print()
 	
 	fmt.Println("Remaining cards:")
 	remainingCards.print()
-
 }
