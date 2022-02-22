f8164986aaefc904544f77246d40e14b6f06ebc0
anaolisilva <ana.oli@cora.com.br>
2022-02-22 17:06:48 -0300

    up to video 21

--- cards/deck.go
+++ cards/deck.go
@@ -1,2 +1,38 @@
 package main
 
+import "fmt"
+
+//Declarando o novo tipo: deck, que funciona como um slice de string.
+type deck []string
+
+//Lembrar que (d deck) é um receiver, que torna a função disponíveis para
+//variáveis do tipo deck.
+func (d deck) print() {
+	for i, singleCard := range d {
+		fmt.Println(i, singleCard)
+	}
+}
+
+//Não tem receiver porque está justamente criando uma variável do tipo deck, ou seja
+//não faria sentido que para criar um novo deck precisássemos de um já pronto.
+//Recebe um parâmetro caso desejem que tenha coringas.
+func newDeck(joker bool) deck {
+	cards := deck{}
+
+	cardSuits := []string{ "Spades", "Diamonds", "Hearts", "Clu"}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	//Iterando pelos nipes e valores para criar todas as combinações de cartas.
+	for _, suit := range cardSuits{
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	if(joker){
+		cards = append(cards, "Joker", "Joker")
+	}
+
+	return cards
+}
--- cards/main.go
+++ cards/main.go
@@ -1,21 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	//Declarando e populando um slice.
-	cards := []string{ newCard(), "Eight of Hearts"}
-	
-	//Adicionando elemento ao slice (não altera, constrói um novo).
-	cards = append(cards, "Six of Spades")
+	//Declarando e populando um novo tipo deck.
+	cards := newDeck(false)
 
-	//Iterando pelo slice de cards (lembrar de usar o index, se não o código não compila)
-	for i, singleCard := range cards {
-		fmt.Println(i, singleCard)
-	}
-}
+	//Chamando a função criada no tipo deck
+	cards.print()
 
-func newCard() string {
-	return "Five of Diamonds"
 }
